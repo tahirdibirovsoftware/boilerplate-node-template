ee9de0153948bac86230d37eef27dee367ca49be
tahirdibirov <tahirdibirovsecond@gmail.com>
2024-10-22 11:20:48 +0400

    INIT

--- src/models/task.model.ts
+++ src/models/task.model.ts
@@ -1,8 +1,9 @@
-import { Document } from 'mongoose';
+import { Document, Schema } from 'mongoose';
 import mongoose from 'mongoose';
 
 export type TaskStatus = 'todo' | 'in_progress' | 'done';
 export type TaskPriority = 'low' | 'medium' | 'high';
+
 
 export interface ITask extends Document {
     title: string;
@@ -14,3 +15,18 @@ export interface ITask extends Document {
     project: mongoose.Types.ObjectId;
     tags: Array<string>
 }
+
+const TaskShcema: Schema<ITask> = new Schema<ITask>({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: String, enum: ['todo', 'in_progress', 'done'], default: 'todo' },
+    priority: { type: String, enum: ['low', 'medium', 'high'] },
+    dueDate: { type: Date },
+    assignedTo: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    project: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
+    tags: [{ type: String }]
+
+}, { timestamps: true });
+
+
+const Task = mongoose.model<ITask>('Task', TaskShcema);
