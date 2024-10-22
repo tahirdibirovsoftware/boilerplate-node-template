64eb3bc3bac83962cae8c52dd301a5a6e9705599
tahirdibirov <tahirdibirovsecond@gmail.com>
2024-10-22 17:12:56 +0400

    Basic server configuration

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -9,10 +9,12 @@ class UserController {
 
     async registerUser(req: Request, res: Response) {
         await this.userService.registerUser(req.body);
+        res.status(200).json(req.body);
     }
 
     async loginUser(req: Request, res: Response): Promise<void> {
         await this.userService.loginUser(req.body);
+        res.status(200).json(req.body);
     }
 
     async logoutUser(): Promise<void> {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,10 +6,12 @@ class UserService {
 
     async registerUser(userData: CreateUserDto): Promise<void> {
         const { email, username, password } = userData;
+        console.log(email, username, password);
     }
     async loginUser(userData: LoginUserDto): Promise<void> {
         const { username, email, password } = userData;
         const userIdentifier = username || email;
+        console.log({ userIdentifier, password })
     }
     async logoutUser(): Promise<void> {
         console.log(`Logged out`);
